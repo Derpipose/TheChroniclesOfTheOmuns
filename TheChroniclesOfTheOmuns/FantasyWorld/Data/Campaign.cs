db31d9f314057731769363f4402a56ba31293a21
Derpipose <thefluffmaster2000@gmail.com>
2024-10-26 17:05:10 -0600

    I did a set up of something

---
+++ TheChroniclesOfTheOmuns/FantasyWorld/Data/Campaign.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FantasyWorld.Data {
+    public class Campaign {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string? Name { get; set; }
+    }
+}
