124c8e9c3321a3adae781bdfca0d95408fbc1cd6
Stormgate1998 <jalapenojosh@gmail.com>
2025-12-13 15:11:09 -0700

    Edited classes to conform to style guide

--- ChroniclesTest/RaceUnitTests.cs
+++ ChroniclesTest/RaceUnitTests.cs
@@ -1,27 +1,26 @@
-﻿using PlayerApp.Models;
+using PlayerApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ChroniclesTest
+namespace ChroniclesTest;
+
+public class RaceUnitTests
 {
-    public class RaceUnitTests
+
+    [Test]
+    public void TestCharacterRace()
     {
-
-        [Test]
-        public void TestCharacterRace()
+        CharacterRace race = new CharacterRace()
         {
-            CharacterRace race = new CharacterRace()
-            {
-                Name = "Human",
-                Description = "Your standard, vanilla humanoid"
-            };
+            Name = "Human",
+            Description = "Your standard, vanilla humanoid"
+        };
 
-            Assert.Multiple(() =>
-            {
-                Assert.That(race.Name, Is.EqualTo("Human"));
-                Assert.That(race.Description, Is.EqualTo("Your standard, vanilla humanoid"));
-            });
-        }
+        Assert.Multiple(() =>
+        {
+            Assert.That(race.Name, Is.EqualTo("Human"));
+            Assert.That(race.Description, Is.EqualTo("Your standard, vanilla humanoid"));
+        });
     }
 }
